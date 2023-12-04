5c628c97627d2942d3e88321f1b979b8dfbdbc2b
Ratheesh R <94684603+RatheeshCoder@users.noreply.github.com>
2023-12-04 09:44:09 +0530

    Update Hiring.jsx

--- src/components/Jobs/Section2/Hiring/Hiring.jsx
+++ src/components/Jobs/Section2/Hiring/Hiring.jsx
@@ -5,7 +5,7 @@ const Hiring = () => {
   return (
     <section className='Hiring'>
         <h1>We are <span>hiring</span></h1>
-        <p>Kopuram is growing, be a part, and grow with us</p>
+        <p className='kopuram'>Kopuram is growing, be a part, and grow with us</p>
         <Cards/>
     </section>
   )
