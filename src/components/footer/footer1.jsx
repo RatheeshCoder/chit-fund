fbc01b0051a82130795acc4bc9a2bea8c0feb54b
RatheeshCoder <rathee.r.ad.2021@snsce.ac.in>
2023-12-05 16:04:59 +0530

    fix the bug and change the design

--- src/components/footer/footer1.jsx
+++ src/components/footer/footer1.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Footer1 = () => {
   return (
-    <section className="py-28 relative bg-blue-600 ">
+    <section className="py-28 relative bg-blue-600 text-center ">
         <div className="relative max-w-screen-xl mx-auto px-4 md:text-center md:px-8">
-            <div className="max-w-xl md:mx-auto">
-                <p className="text-white text-3xl font-semibold sm:text-4xl">
+            <div className="max-w-xl md:mx-auto text-center">
+                <p className=" cta-footer-text text-white text-3xl font-semibold sm:text-4xl">
                     Build the future with us
                 </p>
-                <p className="text-blue-100 mt-3">
+                <p className=" cta-footer-text  text-blue-100 mt-3">
                     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat non proident.
                 </p>
             </div>
             <div className="mt-4">
-                <a href="javascript:void(0)" className="inline-block py-2 px-4 text-gray-800 font-medium bg-white duration-150 hover:bg-gray-100 active:bg-gray-200 rounded-full">
+                <Link to="/Product" className="inline-block py-2 px-4 text-gray-800 font-medium bg-white duration-150 hover:bg-gray-100 active:bg-gray-200 rounded-full">
                     Get started
-                </a>
+                </Link>
             </div>
         </div>
     </section>
