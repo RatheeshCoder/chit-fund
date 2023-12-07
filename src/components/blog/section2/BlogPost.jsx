6d819cfdba486979a055c428dbb7f807866d51a9
RatheeshCoder <rathee.r.ad.2021@snsce.ac.in>
2023-12-07 18:55:27 +0530

    change in the blog page

--- src/components/blog/section2/BlogPost.jsx
+++ src/components/blog/section2/BlogPost.jsx
@@ -27,36 +27,39 @@ const BlogPost = () => {
   }
 
   return (
-    <section className='single-blog'>
-      <div className='cta-blog-img'>
-      <img src={blog.image} alt={blog.title} />
+    <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16 relative">
+      <div
+        className="bg-cover bg-center text-center overflow-hidden"
+        style={{ minHeight: '500px', backgroundImage: `url('${blog.image}')` }}
+        title={blog.title}
+      ></div>
+      <div className="max-w-3xl mx-auto">
+        <div className="mt-3 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
+          <div className="bg-white relative top-0 -mt-32 p-5 sm:p-10">
+            <h1 className="text-gray-900 font-bold text-3xl mb-2">{blog.title_content}</h1>
+            <p className="text-gray-700 text-xs mt-2">
+             
+              <a
+                href="#"
+                className="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out"
+              >
+                {blog.category}
+              </a>
+            </p>
+
+            <p className="text-base leading-8 my-5">{blog.heading_content}</p>
+            <p className="text-base leading-8 my-5">{blog.content1}</p>
+            <p className="text-base leading-8 my-5">{blog.content2}</p>
+            <p className="text-base leading-8 my-5">{blog.content3}</p>
+            <p className="text-base leading-8 my-5">{blog.content4}</p>
+            <p className="text-base leading-8 my-5">{blog.content5}</p>
+            <p className="text-base leading-8 my-5">{blog.content6}</p>
+            <p className="text-base leading-8 my-5">{blog.content7}</p>
+            <p className="text-base leading-8 my-5">{blog.content8}</p>
+          </div>
+        </div>
       </div>
-
-
-     <div className='cta-blog-content'>  
-
-          <div className='cta-story-of-week'>
-            <h1>Story of the Week</h1>
-          </div>
-          
-          <div className='cta-blog-title'>
-            <h3>{blog.title_content}</h3>
-          </div>
-          
-          <div className='cta-main-content'>
-              <p>{blog.heading_content}</p>
-              <p>{blog.content1}</p>
-              <p>{blog.content2}</p>
-              <p>{blog.content3}</p>
-              <p>{blog.content4}</p>
-              <p>{blog.content5}</p>
-              <p>{blog.content6}</p>
-              <p>{blog.content7}</p>
-          </div>
-          
-
-      </div>
-    </section>
+    </div>
   );
 };
 
