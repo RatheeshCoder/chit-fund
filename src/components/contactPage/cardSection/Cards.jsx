312de342d4a16496d7bc13e66d1591e75b276313
RatheeshCoder <rathee.r.ad.2021@snsce.ac.in>
2023-12-10 17:01:48 +0530

    samll change

--- src/components/contactPage/cardSection/Cards.jsx
+++ src/components/contactPage/cardSection/Cards.jsx
@@ -5,7 +5,7 @@ import RoomIcon from "@mui/icons-material/Room";
 const Cards = () => {
   return (
     <>
-      <div className="relative  min-h-screen grid grid-cols-1 px-10  gap-10 overflow-hidden bg-gray-50 py-6 md:grid-cols-2 lg:grid-cols-3 sm:py-12">
+      <div className="relative  min-h-screen grid grid-cols-1 px-10  gap-10 overflow-hidden  py-6 md:grid-cols-2 lg:grid-cols-3 sm:py-12">
         {BranchData.map((branch) => (
           <>
             <div className="group relative cursor-pointer justify-around overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
