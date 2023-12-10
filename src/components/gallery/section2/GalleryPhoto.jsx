6f2999475b6e299b0735ac5c4ec1b2ede57936ac
RatheeshCoder <rathee.r.ad.2021@snsce.ac.in>
2023-12-10 23:53:49 +0530

    change in gallery

--- src/components/gallery/section2/GalleryPhoto.jsx
+++ src/components/gallery/section2/GalleryPhoto.jsx
@@ -1,206 +1,110 @@
-import React, { Component } from "react";
-import Lightbox from "react-image-lightbox";
-import "react-image-lightbox/style.css";
-import photoData from "../../../data/data";
+import React, { useState } from 'react';
+import ImageModal from './ImageModal';
+import photoData from '../../../data/data';
+// import './YourStylesheet.css'; // Import your stylesheet
 
-class GalleryPhoto extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      photos: [],
-      selectedFilter: "all",
-      isOpen: false,
-      photoIndex: 0,
-    };
-  }
+const GalleryPhoto = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [activeCategory, setActiveCategory] = useState(null);
 
-  componentDidMount() {
-    this.setState({
-      photos: photoData,
-    });
-  }
-
-  handleFilterChange = (filter) => {
-    this.setState({ selectedFilter: filter });
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+    setSelectedImage(null); // Reset selected image when changing the category
+    setActiveCategory(category); // Update active category
   };
 
-  openLightbox = (index) => {
-    this.setState({ isOpen: true, photoIndex: index });
+  const handleImageClick = (imageUrl, event) => {
+    event.stopPropagation(); // Stop event propagation to prevent multiple modals from opening
+    setSelectedImage(imageUrl);
   };
 
-  closeLightbox = () => {
-    this.setState({ isOpen: false });
+  const handleCloseModal = () => {
+    setSelectedImage(null);
   };
 
-  movePrev = () => {
-    this.setState((state) => ({
-      photoIndex:
-        (state.photoIndex + state.photos.length - 1) % state.photos.length,
-    }));
+  const handlePrevImage = () => {
+    if (!selectedImage) return;
+
+    const currentIndex = photoData.findIndex((photo) => photo.url === selectedImage);
+    const prevIndex = (currentIndex - 1 + photoData.length) % photoData.length;
+    setSelectedImage(photoData[prevIndex].url);
   };
 
-  moveNext = () => {
-    this.setState((state) => ({
-      photoIndex: (state.photoIndex + 1) % state.photos.length,
-    }));
+  const handleNextImage = () => {
+    if (!selectedImage) return;
+
+    const currentIndex = photoData.findIndex((photo) => photo.url === selectedImage);
+    const nextIndex = (currentIndex + 1) % photoData.length;
+    setSelectedImage(photoData[nextIndex].url);
   };
 
-  render() {
-    const { photos, selectedFilter, isOpen, photoIndex } = this.state;
-    const filteredPhotos = photos.filter(
-      (photo) => selectedFilter === "all" || photo.category === selectedFilter
-    );
+  const filteredPhotos = selectedCategory
+    ? photoData.filter((photo) => photo.category === selectedCategory)
+    : photoData;
 
-    if (filteredPhotos.length === 0) {
-      return (
-        <div className="cta-no-image">
-          <h1>No photos available for the selected filter.</h1>
-        </div>
-      );
-    }
-
-    const currentPhoto = filteredPhotos[photoIndex];
-
-    return (
-      <section className="gallery-photo container mx-auto p-4 md:p-8 lg:p-12">
-        <div className="cta-title text-center mb-4">
-          <h1 className="text-2xl md:text-3xl lg:text-4xl">
-            Be A Part Of Our Joy Faces You See! Moments With Our Financial
-            Geeks!
-          </h1>
-        </div>
-
-        <div className="cta-filter-option flex justify-center space-x-4 mb-4">
-          <button
-            className={`${
-              selectedFilter === "all"
-                ? "bg-blue-500 text-white"
-                : "bg-gray-300 text-gray-700"
-            } px-4 py-2 rounded`}
-            onClick={() => this.handleFilterChange("all")}
+  return (
+    <div>
+       <div className=" my-10 caption text-transparent bg-clip-text bg-gradient-to-r from-blue-700 via-blue-800 to-black ">
+      <h2 className='font-display  tracking-tight lg:text-5xl text-3xl md:text-3xl font-bold tracking-tight'>Be A Part Of Our Joy Faces You See! Moments With Our Financial Geeks</h2>
+    </div>
+      <div className="filter-options">
+        <button
+          onClick={() => handleCategoryClick(null)}
+          className={activeCategory === null ? 'active' : ''}
+        >
+          All
+        </button>
+        <button
+          onClick={() => handleCategoryClick('CustomerMeetups')}
+          className={activeCategory === 'CustomerMeetups' ? 'active' : ''}
+        >
+          Customer Meetups
+        </button>
+        <button
+          onClick={() => handleCategoryClick('FestivalCelebrations')}
+          className={activeCategory === 'FestivalCelebrations' ? 'active' : ''}
+        >
+          Festival Celebrations
+        </button>
+        <button
+          onClick={() => handleCategoryClick('anniversary')}
+          className={activeCategory === 'anniversary' ? 'active' : ''}
+        >
+          Anniversary
+        </button>
+        {/* Add more buttons for other categories as needed */}
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+        {filteredPhotos.map((photo) => (
+          <div
+            key={photo.id}
+            className="group cursor-pointer relative"
+            onClick={(event) => handleImageClick(photo.url, event)}
           >
-            All
-          </button>
-          <button
-            className={`${
-              selectedFilter === "CustomerMeetups"
-                ? "bg-blue-500 text-white"
-                : "bg-gray-300 text-gray-700"
-            } px-4 py-2 rounded`}
-            onClick={() => this.handleFilterChange("CustomerMeetups")}
-          >
-            {selectedFilter === "CustomerMeetups" ? (
-              <span className="text-blue-500">Customer Meetups</span>
-            ) : (
-              "Customer Meetups"
-            )}
-          </button>
-          <button
-            className={`${
-              selectedFilter === "FestivalCelebrations"
-                ? "bg-blue-500 text-white"
-                : "bg-gray-300 text-gray-700"
-            } px-4 py-2 rounded`}
-            onClick={() => this.handleFilterChange("FestivalCelebrations")}
-          >
-            {selectedFilter === "FestivalCelebrations" ? (
-              <span className="text-blue-500">Festival Celebrations</span>
-            ) : (
-              "Festival Celebrations"
-            )}
-          </button>
-          <button
-            className={`${
-              selectedFilter === "anniversary"
-                ? "bg-blue-500 text-white"
-                : "bg-gray-300 text-gray-700"
-            } px-4 py-2 rounded`}
-            onClick={() => this.handleFilterChange("anniversary")}
-          >
-            {selectedFilter === "anniversary" ? (
-              <span className="text-blue-500"> Anniversary celebration</span>
-            ) : (
-              "Anniversary celebration"
-            )}
-          </button>
-        </div>
-
-        <div className="gallery grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
-          {filteredPhotos.map((photo, index) => (
-            <div key={photo.id}>
-              <img
-                className="zoomIn  cursor-pointer"
-                src={photo.url}
-                alt={`Photo ${photo.id}`}
-                onClick={() => this.openLightbox(index)}
-                style={{ margin: "0", display: "block" }}
-              />
+            <img
+              src={photo.url}
+              alt={`Image ${photo.id}`}
+              className="w-full h-48 object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
+            />
+            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
+              <button className="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors">
+                View
+              </button>
             </div>
-          ))}
-        </div>
-
-        {isOpen && currentPhoto && (
-          <Lightbox
-            mainSrc={currentPhoto.url}
-            nextSrc={
-              filteredPhotos[(photoIndex + 1) % filteredPhotos.length].url
-            }
-            prevSrc={
-              filteredPhotos[
-                (photoIndex + filteredPhotos.length - 1) % filteredPhotos.length
-              ].url
-            }
-            onCloseRequest={this.closeLightbox}
-            onMovePrevRequest={this.movePrev}
-            onMoveNextRequest={this.moveNext}
-            imageTitle={`Photo ${photoIndex + 1}`}
-            wrapperClassName="custom-lightbox-wrapper"
-            reactModalStyle={{ overlay: { zIndex: 1100 } }}
-            imageAlt={`Photo ${photoIndex + 1}`}
-            enableZoom={true}
-            shouldCloseOnEsc={true}
-            animationDuration={300}
-            animationDisabled={true}
-            nextLabel="Next"
-            prevLabel="Previous"
-            closeLabel="Close"
-            zoomInLabel="Zoom In"
-            zoomOutLabel="Zoom Out"
-            imagePadding={20}
-            clickOutsideToClose={true}
-            moveNextOnCloseRequest={true}
-            movePrevOnCloseRequest={true}
-            discourageDownloads={true}
-            toolbarButtons={[
-              <button
-                key="close"
-                className="lightbox-button"
-                onClick={this.closeLightbox}
-              >
-                Close
-              </button>,
-              <button
-                key="prev"
-                className="lightbox-button"
-                onClick={this.movePrev}
-                disabled={photoIndex === 0}
-              >
-                Previous
-              </button>,
-              <button
-                key="next"
-                className="lightbox-button"
-                onClick={this.moveNext}
-                disabled={photoIndex === filteredPhotos.length - 1}
-              >
-                Next
-              </button>,
-            ]}
-          />
-        )}
-      </section>
-    );
-  }
-}
+          </div>
+        ))}
+      </div>
+      {selectedImage && (
+        <ImageModal
+          imageUrl={selectedImage}
+          onClose={handleCloseModal}
+          onPrev={handlePrevImage}
+          onNext={handleNextImage}
+        />
+      )}
+    </div>
+  );
+};
 
 export default GalleryPhoto;
