e01ce6fe3a7ae520e44628279941c79425678da2
RatheeshCoder <rathee.r.ad.2021@snsce.ac.in>
2024-02-01 09:12:45 +0530

    update the design

--- Frontend/src/components/home/KeyBenefits/Benefit.jsx
+++ Frontend/src/components/home/KeyBenefits/Benefit.jsx
@@ -5,7 +5,7 @@ import { Benefitcontent, Benefitoptions } from '../../../data/data';
 
 function Benefit() {
   return (
-    <div className='lg:mx-10' >
+    <div className='mx-10 lg:mx-20' >
       <h1 className="mt-10 mb-10 text-4xl font-bold text-center text-gray-800">
       Key <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900' >Benefits</span>
         </h1>
